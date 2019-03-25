c4abc5814d9bb3e36d4033a34e1474b376528270
Tom Eyckmans <teyckmans@gmail.com>
2019-03-25 23:29:58 +0100

    working on publishing to bintray

--- kuberig-dsl-openshift/kuberig-dsl-openshift-3-6/build.gradle.kts
+++ kuberig-dsl-openshift/kuberig-dsl-openshift-3-6/build.gradle.kts
@@ -1,21 +1,7 @@
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.plugins.ide.idea.model.Module
-
-plugins {
-    java
-    kotlin("jvm") version "1.3.20"
-    idea
-}
-
-group = "eu.rigeldev.kuberig.openshift"
-
-repositories {
-    mavenCentral()
-}
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation("com.fasterxml.jackson.core:jackson-core:2.9.8")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
@@ -24,20 +10,7 @@ dependencies {
 
     implementation(project(":kuberig-dsl-base"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
     testImplementation("org.skyscreamer:jsonassert:1.5.0")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
 }
 
 sourceSets["main"].withConvention(KotlinSourceSet::class) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,3 @@ rootProject.name = "kuberig-dsl"
 
 include ("kuberig-dsl-generator")
 include ("kuberig-dsl-base")
-
-include ("kuberig-dsl-kubernetes:kuberig-dsl-kubernetes-1-14")
-include("kuberig-dsl-openshift:kuberig-dsl-openshift-3-6")
