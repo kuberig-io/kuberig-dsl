301654d3a322e034dc83b43171467e573f80b1b6
Tom Eyckmans <teyckmans@gmail.com>
2019-10-09 20:44:59 +0200

    remove old dsl sub-project directories
    - cleaned up commented out sub-project include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,3 @@ rootProject.name = "kuberig-dsl"
 include ("kuberig-dsl-generator")
 include ("kuberig-dsl-base")
 include ("kuberig-dsl-generator-gradle-plugin")
-
-//include ("kuberig-dsl-kubernetes")
