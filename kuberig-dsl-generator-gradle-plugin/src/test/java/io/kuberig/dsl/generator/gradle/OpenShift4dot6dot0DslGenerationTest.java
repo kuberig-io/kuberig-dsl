d7edd627a8af4b85fa90ff1855d7fc16b744f0e5
Tom Eyckmans <teyckmans@gmail.com>
2020-10-25 22:48:00 +0100

    #17 oc v4.6.0 generation fixes
    - wip fix test OOM issue.

--- kuberig-dsl-generator-gradle-plugin/src/test/java/io/kuberig/dsl/generator/gradle/OpenShift4dot6dot0DslGenerationTest.java
+++ kuberig-dsl-generator-gradle-plugin/src/test/java/io/kuberig/dsl/generator/gradle/OpenShift4dot6dot0DslGenerationTest.java
@@ -2,19 +2,10 @@ package io.kuberig.dsl.generator.gradle;
 
 import org.junit.jupiter.api.Test;
 
-public class OpenShiftDslGenerationTest extends AbstractDslGenerationTest {
+public class OpenShift4dot6dot0DslGenerationTest extends AbstractDslGenerationTest {
 
-    public OpenShiftDslGenerationTest() {
+    public OpenShift4dot6dot0DslGenerationTest() {
         super("openshift");
-    }
-
-    /**
-     * From OpenShift 3.7.0 onwards the OpenShift api specifications also contain the kind meta data in all lower
-     * case like in the Kubernetes api specifications.
-     */
-    @Test
-    public void testOpenShiftVersion3dot11dot0() throws Exception {
-        this.attemptDslGenerationAndProjectCompilation("3.11.0");
     }
 
     /**
