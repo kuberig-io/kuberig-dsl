03718b2fae2254b6ec53ca07274c71b752270a91
Tom Eyckmans <teyckmans@gmail.com>
2021-03-24 00:09:53 +0100

    switch to gitlab ci

--- ci-gradle.sh
+++ ci-gradle.sh
@@ -1,5 +1,5 @@
 source ci-gradle-init.sh
-./gradlew \
+gradle \
   -Pgradle.publish.key="${GRADLE_PUBLISH_KEY}" \
   -Pgradle.publish.secret="${GRADLE_PUBLISH_SECRET}" \
   -PsonatypeUsername="${SONATYPE_USERNAME}" \
