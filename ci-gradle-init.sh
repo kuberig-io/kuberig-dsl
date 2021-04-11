863ce168edb1fe0679d18dc0255bf0e1e947b95b
tom.eyckmans <teyckman@its.jnj.com>
2021-04-11 20:42:36 +0200

    remove gitlab ci files

--- ci-gradle-init.sh
+++
@@ -1,7 +0,0 @@
-export GRADLE_USER_HOME=`pwd`/.gradle
-mkdir -p $GRADLE_USER_HOME
-export PLAIN_M2_SIGNING_KEY=${GRADLE_USER_HOME}/m2_signing_key.pgp
-rm -rf $PLAIN_M2_SIGNING_KEY
-echo $M2_SIGNING_KEY | base64 -d > ${PLAIN_M2_SIGNING_KEY}
-export GRADLE_OPTS='-Dorg.gradle.daemon=false -Dorg.gradle.caching=true -Dorg.gradle.parallel=true -Dorg.gradle.jvmargs="-Xmx4g -XX:MaxMetaspaceSize=1g"'
-if [ -z "$CI_COMMIT_TAG" ]; then git checkout -B $CI_COMMIT_REF_NAME $CI_COMMIT_SHA; fi
