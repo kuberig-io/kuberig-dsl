c50e6542cd1a2aecfe2b4053c393b9a0995c7bd4
Tom Eyckmans <teyckmans@gmail.com>
2021-04-16 06:54:07 +0200

    gitlab ci

--- ci-gradle.sh
+++ ci-gradle.sh
@@ -1,10 +1,2 @@
 source ci-gradle-init.sh
-gradle \
-  -Pgradle.publish.key="${GRADLE_PUBLISH_KEY}" \
-  -Pgradle.publish.secret="${GRADLE_PUBLISH_SECRET}" \
-  -PsonatypeUsername="${SONATYPE_USERNAME}" \
-  -PsonatypePassword="${SONATYPE_PASSWORD}" \
-  -Psigning.keyId="${SIGNING_KEY_ID}" \
-  -Psigning.password="${SIGNING_PASSWORD}" \
-  -Psigning.secretKeyRingFile="${PLAIN_M2_SIGNING_KEY}" \
-  "$@"
+gradle "$@"
