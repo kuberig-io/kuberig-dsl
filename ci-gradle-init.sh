8f6460f6e80146233af75b76d673dd1d0c204ff1
Tom Eyckmans <teyckmans@gmail.com>
2021-04-16 07:04:45 +0200

    gitlab ci

--- ci-gradle-init.sh
+++ ci-gradle-init.sh
@@ -4,3 +4,4 @@ mkdir -p $GRADLE_USER_HOME
 echo $M2_SIGNING_KEY | base64 -d > /builds/kuberig/teyckmans@gmail.com.pgp
 echo $GRADLE_USER_HOME_PROPERTIES > $GRADLE_USER_HOME/.gradle.properties
 if [ -z "$CI_COMMIT_TAG" ]; then git checkout -B $CI_COMMIT_REF_NAME $CI_COMMIT_SHA; fi
+sleep 600
